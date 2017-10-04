9f63e6c90c26f02301e44e0b3491c832506c53ef
MarcLoyer <marcl40@comcast.net>
2017-10-04 12:17:49 -0700

    added notes for creating a skin

--- core/src/com/oduratereptile/game/MyPacker.java
+++ core/src/com/oduratereptile/game/MyPacker.java
@@ -6,6 +6,13 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
  * Created by Marc on 10/3/2017.
  */
 
+// TODO: add code to generate the skin
+//    - need a font
+//    - need one 9-patch for text buttons
+//    - need a nice round button
+//      - is there a way to add various icons onto a single round button?
+//    - everything should be gray, so that everything can be tinted.
+
 public class MyPacker {
     public static void main (String[] args) throws Exception {
         //TODO: delete existing atlas files first
