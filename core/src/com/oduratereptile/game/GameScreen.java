150230a40c0ffe62be6381ca1bd3bdc2824e1f3a
MarcLoyer <marcl40@comcast.net>
2017-10-05 15:40:10 -0700

    added a status line

--- core/src/com/oduratereptile/game/GameScreen.java
+++ core/src/com/oduratereptile/game/GameScreen.java
@@ -3,22 +3,24 @@ package com.oduratereptile.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import static com.badlogic.gdx.scenes.scene2d.ui.Table.Debug.actor;
 
 /**
  * Created by Marc on 10/3/2017.
  */
 
 public class GameScreen implements Screen {
-    public OrthographicCamera camera;
     final public MyPuzzle game;
 
     public Stage stage;
@@ -27,12 +29,10 @@ public class GameScreen implements Screen {
     public Table table_ur;
     public Table table_ll;
     public Table table_lr;
+    public Label status;
 
     public GameScreen(final MyPuzzle game) {
         this.game = game;
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, MyPuzzle.SCREENSIZEX, MyPuzzle.SCREENSIZEY);
 
         build_hud();
     }
@@ -40,7 +40,7 @@ public class GameScreen implements Screen {
     public void build_hud() {
         stage = new Stage(new FitViewport(MyPuzzle.SCREENSIZEX, MyPuzzle.SCREENSIZEY));
         Gdx.input.setInputProcessor(stage); // TODO: change this to input multiplexer
-//        stage.setDebugAll(true);
+        stage.setDebugAll(true);
 
         table_root = new Table();
         table_root.setFillParent(true);
@@ -64,6 +64,12 @@ public class GameScreen implements Screen {
         table_lr.right().bottom();
         table_root.add(table_lr).right().bottom().expand();
 
+        table_root.row();
+
+        status = new Label("... test ...\n1 .. 2 ... 3 ... 4", game.skin);
+        status.setAlignment(Align.center);
+        table_root.add(status).colspan(2).expandX().fillX();
+
         ImageButton ibutton = new ImageButton(game.skin, "blue");
         ibutton.addListener(new ClickListener(){
             @Override
@@ -73,6 +79,22 @@ public class GameScreen implements Screen {
             }
         });
         table_lr.add(ibutton).width(40f).height(40f);
+    }
+
+    public enum Corner {UL, UR, LL, LR};
+
+    public <T extends Actor> Cell<T> addToHud(Corner corner, T actor) {
+        switch(corner) {
+            case UL : return table_ul.add(actor);
+            case UR : return table_ur.add(actor);
+            case LL : return table_ll.add(actor);
+            case LR : return table_lr.add(actor);
+        }
+        return null;
+    }
+
+    public void setStatus(String s) {
+
     }
 
     @Override
@@ -85,15 +107,8 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0.2f, 0, 0, 1); // TODO: I changed the background color just to tell the difference from the MainMenuScreen
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
-
         stage.act(delta);
         stage.draw();
-
-        game.batch.begin();
-        game.batch.end();
-
     }
 
     @Override
