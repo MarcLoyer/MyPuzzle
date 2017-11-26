5387180a34bd494a5499eb9cfc3fea568c788a5e
MarcLoyer <marcl40@comcast.net>
2017-11-25 18:55:32 -0800

    support auto-rotation

--- core/src/com/oduratereptile/game/HudScreen.java
+++ core/src/com/oduratereptile/game/HudScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -15,6 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by Marc on 10/3/2017.
@@ -144,6 +146,12 @@ public class HudScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
+        float aspectRatio = (float)height / (float)width;
+        if (aspectRatio > 1f) { // portrait
+            stage.getViewport().setWorldSize(MyPuzzle.SCREENSIZEY, MyPuzzle.SCREENSIZEX);
+        } else { // landscape
+            stage.getViewport().setWorldSize(MyPuzzle.SCREENSIZEX, MyPuzzle.SCREENSIZEY);
+        }
         stage.getViewport().update(width, height, true);
     }
 
