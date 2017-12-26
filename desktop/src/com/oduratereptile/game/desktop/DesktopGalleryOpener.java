1abc9aaf1e0c866e9e3abf60ead88cddf7c79919
MarcLoyer <marcl40@comcast.net>
2017-12-26 11:45:55 -0800

    implemented DesktopGalleryOpener

--- desktop/src/com/oduratereptile/game/desktop/DesktopGalleryOpener.java
+++ desktop/src/com/oduratereptile/game/desktop/DesktopGalleryOpener.java
@@ -3,22 +3,48 @@ package com.oduratereptile.game.desktop;
 import com.badlogic.gdx.Gdx;
 import com.oduratereptile.game.GalleryOpener;
 
+import java.io.File;
 import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
 
 /**
  * Created by Marc on 11/19/2017.
  */
 
 public class DesktopGalleryOpener implements GalleryOpener {
+    public JFileChooser fileChooser;
+    public File file = null;
+    public FileDescriptor fd = null;
+
+    public DesktopGalleryOpener() {
+        fileChooser = new JFileChooser();
+        fileChooser.addChoosableFileFilter(new ImageFilter());
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setAccessory(new ImagePreview(fileChooser));
+        fileChooser.setCurrentDirectory(new File("C:\\Users\\Marc\\Pictures"));
+    }
+
     @Override
     public void openGallery() {
-        Gdx.app.log("DesktopGalleryOpener", "Not implemented");
+        int returnValue = fileChooser.showOpenDialog(null);
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            file = fileChooser.getSelectedFile();
+            try {
+                FileInputStream fis = new FileInputStream(file);
+                fd = fis.getFD();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public String getSelectedFilePath() {
-        Gdx.app.log("DesktopGalleryOpener", "Not implemented");
-        return null;
+        return file.getName();
     }
 
     @Override
@@ -29,7 +55,6 @@ public class DesktopGalleryOpener implements GalleryOpener {
 
     @Override
     public boolean resultIsReady() {
-        Gdx.app.log("DesktopGalleryOpener", "Not implemented");
-        return false;
+        return (file != null);
     }
 }
---
+++ desktop/src/com/oduratereptile/game/desktop/ImageFilter.java
@@ -0,0 +1,40 @@
+package com.oduratereptile.game.desktop;
+
+/**
+ * Created by Marc on 12/26/2017.
+ */
+
+import java.io.File;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+
+public class ImageFilter extends FileFilter {
+
+    //Accept all directories and all gif, jpg, tiff, or png files.
+    public boolean accept(File f) {
+        if (f.isDirectory()) {
+            return true;
+        }
+
+        String extension = Utils.getExtension(f);
+        if (extension != null) {
+            if (extension.equals(Utils.tiff) ||
+                    extension.equals(Utils.tif) ||
+                    extension.equals(Utils.gif) ||
+                    extension.equals(Utils.jpeg) ||
+                    extension.equals(Utils.jpg) ||
+                    extension.equals(Utils.png)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    //The description of this filter
+    public String getDescription() {
+        return "Just Images";
+    }
+}
